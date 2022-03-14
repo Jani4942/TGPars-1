8a7998659db7d3de014022dd41390c135e97380f
djq47r <65504612+djq47r@users.noreply.github.com>
2022-03-14 13:55:23 +0300

    Add files via upload

--- pars.py
+++ pars.py
@@ -1,48 +1,48 @@
 @@ -1,64 +1,64 @@
-из telethon.sync импорт TelegramClient
-из telethon.tl.functions.messages импорт GetDialogsRequest
-из telethon.tl.types import InputPeerEmpty
-импорт os, sys
-импорт configparser
-импорт csv
- время импорта
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+import os, sys
+import configparser
+import csv
+ importtime
 re="\033[1;31m"
 gr="\033[1;32m"
 cy="\033[1;36m"
 def banner():
     print(f"""
-{re}╔ ╦ ╗{cy}┌─┐┌─┐┌─┐┌─┐┬─┐{re}╔ ═ ╗ 
-{re} ║ {cy}├─┐├┤ ├─┘├─┤├┬┘{re}╚ ═ ╗ 
-{re} ╩ {cy}└─┘└─┘┴ ┴ ┴┴└─{re}╚ ═ ╝ 
-автор https://github.com/elizhabs
+{re}╔╦╗{cy}┌─┐┌─┐┌─┐┌─┐┬─┐{re}╔═╗
+{re} ║ {cy}├─┐├┤ ├─┘├─┤├┬┘{re}╚═╗
+{re} ╩ {cy}└─┘└─┘┴ ┴ ┴┴└─{re}╚═╝
+by https://github.com/elizhabs
         """)
 cpass = configparser.RawConfigParser()
 cpass.read('config.data')
-попробуйте:
+try:
     api_id = cpass['cred']['id']
     api_hash = cpass['cred']['hash']
     phone = cpass['cred']['phone']
-    клиент = TelegramClient(phone, api_id, api_hash)
-кроме KeyError:
+    client = TelegramClient(phone, api_id, api_hash)
+except KeyError:
     os.system('clear')
-    баннер()
-    print(re+"[!] run python3 setup.py первый !!\n")
+    banner()
+    print(re+"[!] run python3 setup.py first !!\n")
     sys.exit(1)
 client.connect()
-если нет client.is_user_authorized():
-    client.send_code_request(телефон)
+if not client.is_user_authorized():
+    client.send_code_request(phone)
     os.system('clear')
-    баннер()
-    client.sign_in(phone, input(gr+'[+] Введите код: '+re))
+    banner()
+    client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
  
 os.system('clear')
-баннер()
-чаты = []
-last_date = Нет
+banner()
+chats = []
+last_date = None
 chunk_size = 200
-группы=[]
+groups=[]
  
-результат = клиент(GetDialogsRequest(
+result = client(GetDialogsRequest(
              offset_date=last_date,
              offset_id=0,
              offset_peer=InputPeerEmpty(),
@@ -51,47 +51,47 @@ chunk_size = 200
          ))
 chats.extend(result.chats)
 
-для чата в чатах:
-    попробуйте:
-        если chat.megagroup== True:
+for chat in chats:
+    try:
+        if chat.megagroup== True:
         #if chat.megagroup== True:
-            groups.append(чат)
-    кроме:
-        продолжить
+            groups.append(chat)
+    except:
+        continue
  
-print(gr+'[+] Выберите группу для очистки участников :'+re)
+print(gr+'[+] Choose a group to scrape members :'+re)
 i=0
-для g в группах:
+for g in groups:
     print(gr+'['+cy+str(i)+gr+']'+cy+' - '+ g.title)
     i+=1
  
 print(")
-g_index = input(gr+"[+] Введите число : "+re)
-target_group=группы[int(g_index)]
+g_index = input(gr+"[+] Enter a Number : "+re)
+target_group=groups[int(g_index)]
  
 print(gr+'[+] Fetching Members...')
 time.sleep(1)
 all_participants = []
 all_participants = client.get_participants(target_group, aggressive=True)
  
-print(gr+'[+] Сохранение в файле...')
+print(gr+'[+] Saving In file...')
 time.sleep(1)
-с open("members.csv","w",encoding='UTF-8') как f:
+with open("members.csv","w",encoding='UTF-8') as f:
     writer = csv.writer(f,delimiter=",",lineterminator="\n")
-    writer.writerow(['имя пользователя','идентификатор пользователя', 'хэш доступа','имя','группа', 'идентификатор группы'])
-    для пользователя в all_participants:
-        если пользователь.имя пользователя:
-            имя пользователя= user.username
-        ещё:
-            имя пользователя= ""
-        если user.first_name:
+    writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
+    for user in all_participants:
+        if user.username:
+            username= user.username
+        else:
+            username= ""
+        if user.first_name:
             first_name= user.first_name
-        ещё:
+        else:
             first_name= ""
-        если user.last_name:
+        if user.last_name:
             last_name= user.last_name
-        ещё:
+        else:
             last_name= ""
         name= (first_name + ' ' + last_name).strip()
-        writer.writerow([имя пользователя,user.id,user.access_hash,name,target_group.title, target_group.id]) 
+        writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
 print(gr+'[+] Members scraped successfully.')
